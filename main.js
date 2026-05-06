8f3d968ad77f5f73b636972376f136b0dac247e5
s0m1k111 <spavel1207@gmail.com>
2026-05-06 14:34:49 +0300

    Step 10: dynamic DOM element creation

---
+++ main.js
@@ -0,0 +1,64 @@
+console.log(document);
+const title = document.getElementById("title");
+const text = document.querySelector(".text");
+console.log(title);
+console.log(text);
+title.textContent = "DOM изменен через JS!";
+title.style.color = "blue";
+title.style.background = "#f0f0f0";
+title.style.padding = "10px";
+text.style.fontSize = "18px";
+text.style.fontStyle = "italic";
+text.style.color = "darkgreen";
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  alert("Кнопка-нажопка!");
+  btn.style.backgroundColor = "#4CAF50";
+  btn.style.color = "white";
+});
+const input = document.getElementById("nameInput");
+const button = document.getElementById("showName");
+const output = document.getElementById("output");
+button.addEventListener("click", () => {
+  if (input.value.trim() !== ""){
+    output.textContent = `Здарова, ${input.value}!`;
+    output.style.color = "green";
+  } else {
+    output.textContent = `Введи имя пэжэ!`;
+    output.style.color = "red";
+  }
+});
+const clearButton =document.getElementById("clearButton");
+clearButton.addEventListener("click", () => {
+  input.value = "";
+  output.textContent = "";
+});
+output.style.color = "";
+input.focus();
+const form = document.getElementById("userForm");
+const usernameInput = document.getElementById("username");
+const ageInput = document.getElementById("age");
+const result = document.getElementById("formResult");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = usernameInput.value.trim();
+  const age = Number(ageInput.value);
+  result.textContent = `Имя: ${name}, Возраст:${age}`;
+  if (name ===""){
+  result.textContent = "Ошибка: Имя не может быть пустым!";
+  result.style.color = "red";
+  usernameInput.focus();
+  return;
+}
+if (isNaN(age) || age <= 0 || age > 120) {
+  result.textContent = "Ошибка: Введите корректный возраст (от 1 до 120)!";
+  result.style.color = "red";
+  ageInput.focus();
+  ageInput.value = "";
+  return;
+} 
+result.textContent = `Данные сохранены: ${name}, ${age} лет`;
+result.style.color = "green";
+form.reset();
+
+});
