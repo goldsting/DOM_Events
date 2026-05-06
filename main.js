fe45b489763035d143744c46491afde90701a122
trazll <degleted@gmail.com>
2026-05-06 12:42:24 +0300

    Step 5: click event handling

--- main.js
+++ main.js
@@ -10,3 +10,9 @@ title.style.padding = "10px";
 text.style.fontSize = "18px";
 text.style.fontStyle = "italic";
 text.style.color = "darkgreen";
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  alert("Кнопка нажопа!");
+  btn.style.backgroundColor = "#4CAF50";
+  btn.style.color = "white";
+});
